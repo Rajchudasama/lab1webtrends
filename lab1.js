aad0ff1bb39f628695b9bffb257672b405f419ae
raj <rajchudasam@outlook.com>
2018-05-11 14:22:42 -0400

    loopchech function checking var keyword

--- lab1.js
+++ lab1.js
@@ -3,3 +3,12 @@ function multiply(num) {
     return num * 2;
 }
 console.log(multiply(num));
+function addition(num) {
+    return num + 10;
+}
+console.log(addition(num));
+function loopcheck() {
+    for (var i = 0; i < 10; i++) {
+    }
+    console.log(i);
+}
--- lab1.ts
+++ lab1.ts
@@ -6,3 +6,10 @@ console.log(multiply(num));
 function addition(num:number):number{
     return num+10;
 }
+console.log(addition(num));
+function loopcheck():void{
+    for(var i=0;i<10;i++){
+
+    }
+    console.log(i);
+}
